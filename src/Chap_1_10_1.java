b3dba895f748a77844dba6ff967f050cb9431fee
aoitech <aoi19900722@yahoo.co.jp>
2023-03-11 21:08:46 +0900

    nextLineメソッドの基礎学習

---
+++ src/Chap_1_10_1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Chap_1_10_1 {
+    public static void main(String[] args) {
+        System.out.println("入力してください");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+    }
+}
