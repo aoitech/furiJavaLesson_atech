4e94bbc667d2de46f481bb11ea1bd9f53ba05eb2
aoitech <aoi19900722@yahoo.co.jp>
2023-03-12 12:04:31 +0900

    printlnの練習

---
+++ src/Chap1_11_1.java
@@ -0,0 +1,9 @@
+public class Chap1_11_1 {
+    public static void main(String[] args) {
+        String num = "30";
+        System.out.println( num + 20);
+        System.out.println(
+                Integer.parseInt( num ) + 20
+        );
+    }
+}
